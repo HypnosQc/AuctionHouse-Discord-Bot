283d43a5232c58d36ffe640819beb649e4d5d453
Mithras <Qc_Angelz@hotmail.fr>
2024-02-19 18:53:00 -0500

    Add files via upload

---
+++ AuctionHouse_Bot.lua
@@ -0,0 +1,45 @@
+local auctionConfig = {
+    auctionWebhookURL = "YOUR_WEBHOOK_URL",
+    lastCheckedTimestamp = 0
+}
+
+local function SendDiscordMessage(message, webhookURL)
+    local postData = '{"content": "'..message..'"}'
+    local curlCommand = 'curl -X POST -H "Content-Type: application/json" -d @- '..webhookURL
+    local curlProcess = io.popen(curlCommand, 'w')
+    curlProcess:write(postData)
+    curlProcess:close()
+end
+
+local function ConvertCopperToGoldSilverCopper(copper)
+    local gold = math.floor(copper / 10000)
+    local remaining = copper % 10000
+    local silver = math.floor(remaining / 100)
+    local remainingCopper = remaining % 100
+    return gold, silver, remainingCopper
+end
+
+local function ConvertSecondsToReadableTime(seconds)
+    local hours = math.floor(seconds / 3600)
+    local minutes = math.floor((seconds % 3600) / 60)
+    return hours, minutes
+end
+
+local function OnAuctionAdd(event, auctionId, owner, item, expireTime, buyout, startBid, currentBid, bidderGUIDLow)
+	local remainingTime = expireTime - os.time()
+
+    local goldBid, silverBid, copperBid = ConvertCopperToGoldSilverCopper(startBid)
+    local priceStringBid = string.format("[%d Gold %02d Silver %02d Copper]", goldBid, silverBid, copperBid)
+
+    local goldBuyout, silverBuyout, copperBuyout = ConvertCopperToGoldSilverCopper(buyout)
+    local priceStringBuyout = string.format("[%d Gold %02d Silver %02d Copper]", goldBuyout, silverBuyout, copperBuyout)
+
+    local hours, minutes = ConvertSecondsToReadableTime(remainingTime)
+    local timeString = remainingTime > 0 and string.format("%d hours %d minutes", hours, minutes) or "Auction ended"
+
+    local message = string.format("[%s] has listed [%s] for %s with a buyout price of %s. Time left: %s", owner:GetName(), item:GetName(), priceStringBid, priceStringBuyout, timeString)
+	SendDiscordMessage(message, auctionConfig.auctionWebhookURL)
+end
+
+-- Register the event handler for AUCTION_EVENT_ON_ADD
+RegisterServerEvent(26, OnAuctionAdd)
